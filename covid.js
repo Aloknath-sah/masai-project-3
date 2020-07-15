5529cf355902ede09208b109e58d11e4c68b5467
Aloknath-sah <kumaraditya6396@gmail.com>
2020-07-15 18:49:09 +0530

    adding another api

--- covid.js
+++ covid.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   var button = document.getElementById("summary");
   button.addEventListener("click", handleSummary);
+
+  var country = document.getElementById("country");
+  country.addEventListener("click", handleCountry);
 };
 
 function handleSummary() {
@@ -19,19 +22,19 @@ function displayData(response) {
   var div = document.getElementById("d_summary");
   var data = summary.Countries;
   console.log(data);
-  var tbody = document.createElement("tbody");
 
   for (var i = 0; i < data.length; i++) {
+    var tbody = document.createElement("tbody");
     var cont = document.createElement("tr");
 
     var country = document.createElement("th");
     country.textContent = data[i].Country;
-    console.log(country);
+    //console.log(country);
     cont.append(country);
 
     var new_confirmed = document.createElement("th");
     new_confirmed.textContent = data[i].NewConfirmed;
-    console.log(new_confirmed);
+    //console.log(new_confirmed);
     cont.append(new_confirmed);
 
     var new_deaths = document.createElement("th");
@@ -45,9 +48,27 @@ function displayData(response) {
     var total_deaths = document.createElement("th");
     total_deaths.textContent = data[i].TotalDeaths;
     cont.append(total_deaths);
+
+    tbody.append(cont);
   }
 
   console.log(cont);
-  tbody.append(cont);
+
   div.append(tbody);
+}
+
+function handleCountry() {
+  var c = document.getElementById("c").value;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.covid19api.com/total/country/" + c);
+  xhr.send();
+
+  xhr.onload = function () {
+    displayStatus(this.response);
+  };
+}
+
+function displayStatus(response) {
+  console.log(response);
 }
